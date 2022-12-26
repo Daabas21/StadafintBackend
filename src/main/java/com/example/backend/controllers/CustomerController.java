17cad957e3fd444ad41ce7a7212e4a301fe5861a
martinacurl <martinacurl@hotmail.com>
2022-12-26 12:32:40 +0100

    added post route for customer bookings

--- src/main/java/com/example/backend/controllers/CustomerController.java
+++ src/main/java/com/example/backend/controllers/CustomerController.java
@@ -3,11 +3,13 @@ package com.example.backend.controllers;
 import com.example.backend.entities.Booking;
 import com.example.backend.entities.Customer;
 
+import com.example.backend.services.BookingService;
 import com.example.backend.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.print.Book;
 import java.util.List;
 
 @CrossOrigin
@@ -17,6 +19,9 @@ public class CustomerController {
 
     @Autowired
     CustomerService customerService;
+
+    @Autowired
+    BookingService bookingService;
 
     @GetMapping("/{id}")
     public Customer findById(@PathVariable int id, Authentication auth) {
@@ -36,6 +41,11 @@ public class CustomerController {
     @PutMapping("/{id}")
     public Customer updateCustomerById(@PathVariable int id, @RequestBody Customer customer, Authentication auth){
         return customerService.updateCustomerById(id, customer, auth);
+    }
+
+    @PostMapping("/booking")
+    public Booking insertNewBooking(@RequestBody Booking booking) {
+        return bookingService.insertNewBooking(booking);
     }
 
 
